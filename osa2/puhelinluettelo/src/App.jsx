b00e6c5544e8f607bcf197396e1b7be86d0d6797
Valtteri Sivula <valtteri15.sivula@gmail.com>
2024-06-20 15:08:12 +0300

    tehtava 2.11 valmis

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Person = ({person}) => 
   <p> {person.name} {person.number} </p>
@@ -38,19 +39,24 @@ const Filter = ({filterValue, handleFilterChange}) =>
       </form>
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filterValue, setNewFilterValue] = useState('')
 
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')   // axios.get = promise
+      .then(response => {                     // then:issä oleva funktio on tapahtumankäsittelijä
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }, [])
+  console.log('render', persons.length, 'persons')
+
   const addPerson = (event) => {
     event.preventDefault()
-    console.log('ennen ', persons)
     if (persons.find(person => person.name === newName)) {
       alert(`${newName} is already added to phonebook`)
     } else {
@@ -65,17 +71,17 @@ const App = () => {
   }
 
   const handleNameChange = (event) => {
-    console.log(event.target.value)
+    //console.log(event.target.value)
     setNewName(event.target.value)
   }
 
   const handleNumberChange = (event) => {
-    console.log(event.target.value)
+    //console.log(event.target.value)
     setNewNumber(event.target.value)
   }
 
   const handleFilterChange = (event) => {
-    console.log('filter: ', event.target.value)
+    //console.log('filter: ', event.target.value)
     setNewFilterValue(event.target.value)
   }
 
@@ -88,8 +94,12 @@ const App = () => {
       <h2>Phonebook</h2>
       <Filter filterValue={filterValue} handleFilterChange={handleFilterChange}/>
       <h2>add a new</h2>
-      <PersonForm addPerson={addPerson} newName={newName} handleNameChange={handleNameChange}
-                  newNumber={newNumber} handleNumberChange={handleNumberChange}/>
+      <PersonForm addPerson={addPerson} 
+                  newName={newName} 
+                  handleNameChange={handleNameChange}
+                  newNumber={newNumber} 
+                  handleNumberChange={handleNumberChange}
+      />
       <h2>Numbers</h2>
       <Persons persons={personsToShow}/>
     </div>
